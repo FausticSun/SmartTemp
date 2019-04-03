e6547b0809dc5e5c4c604fa6ac46f87a12f041d0
FausticSun <FausticSun@gmail.com>
2019-04-03 11:53:03 +0800

    Scaffold temperature pubsub

--- imports/api/collections/Temperatures.js
+++ imports/api/collections/Temperatures.js
@@ -1,5 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 const Temperatures = new Mongo.Collection('Temperatures');
+
+if (Meteor.isServer) {
+  Meteor.publish('Temperatures', (dateTimeRange, visibleRooms, samples) => {
+    return Temperatures.find();
+  });
+}
 
 export default Temperatures;
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Temperatures from '../api/collections/Temperatures';
 
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleRooms: [1, 2, 3, 4, 5, 6],
+      dateTimeRange: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Meteor!</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
